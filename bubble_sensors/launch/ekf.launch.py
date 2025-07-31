6f6dab1968cf57fbb314fa8124051af88d9eccbd
Henry Adam <r.h.adam@student.tudelft.nl>
2025-07-31 16:03:16 +0000

    Changed quotes and disabled auto-double quotes

--- bubble_sensors/launch/ekf.launch.py
+++ bubble_sensors/launch/ekf.launch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-'''
+"""
 This is the Launch file for the EKF node in the robot_localization package in ros2.
 
 This node will take in the ekf.yaml file in the config folder of bubble_sensors,
 and should read from the DVL, the
-'''
+"""
 
 import os
 
