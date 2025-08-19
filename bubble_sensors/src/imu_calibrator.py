45ff0db454ce1853885298c1a0f51cddff745244
Henry Adam <r.h.adam@student.tudelft.nl>
2025-08-19 13:34:08 +0200

    Added Custom IMU Config Service and Messages

--- bubble_sensors/src/imu_calibrator.py
+++ bubble_sensors/src/imu_calibrator.py
@@ -32,7 +32,7 @@ class VN100Configurator(Node):
         super().__init__('imu_calibrator')
 
         # declare the parameters for the ports and baudrate
-        self.declare_parameter("imu_port", '/dev/ttyAMA3')
+        self.declare_parameter("imu_port", '/dev/ttyAMA5')
         self.declare_parameter('kf_port', '/dev/ttyAMA4')
         self.declare_parameter('baudrate', 115200)
 
@@ -63,7 +63,16 @@ class VN100Configurator(Node):
         timeout = time.time() + self.averaging_time  
         while time.time() < timeout:
             line = self.ser_imu.readline().decode('ascii', errors='ignore').strip()
-            if line.startswith("$VNCMV"):  
+            if line.startswith("$VNYMR"):  
+                try:
+                    parts = line.split(',')
+                    # VNCMV fields: magX, magY, magZ, accelX, accelY, accelZ, gyroX, gyroY, gyroZ, temp, pres
+                    ax, ay, az = map(float, parts[7:10])
+                    accel_samples.append([ax, ay, az])
+                except Exception as e:
+                    self.get_logger().error(f"ERROR IN CALIBRATION: {e}")
+                    continue
+            elif line.startswith("$VNCMV"):  
                 try:
                     parts = line.split(',')
                     # VNCMV fields: magX, magY, magZ, accelX, accelY, accelZ, gyroX, gyroY, gyroZ, temp, pres
@@ -72,11 +81,14 @@ class VN100Configurator(Node):
                 except Exception as e:
                     self.get_logger().error(f"ERROR IN CALIBRATION: {e}")
                     continue
+              
+            
 
         if len(accel_samples) < self.min_samples:
             response.success = False
             response.message = "Not enough samples collected"
-            return response
+            accel_samples.append([0,0,0])
+            #return response
 
         avg_accel = np.mean(accel_samples, axis=0)
         self.get_logger().info(f"Averaged accel vector = {avg_accel}")
@@ -87,24 +99,30 @@ class VN100Configurator(Node):
         g = avg_accel.tolist()
         payload = f"VNWRG,21,{mag_ref[0]:.4f},{mag_ref[1]:.4f},{mag_ref[2]:.4f},{g[0]:.4f},{g[1]:.4f},{g[2]:.4f}"
         self.ser_imu.write(full_vn_message(payload))
-        ack = self.ser_imu.readline().decode('ascii', errors='ignore').strip()
-        self.get_logger().info(f"Set gravity vector response: {ack}")
+        waiting_for_response = True
+        while waiting_for_response: 
+            response_line = self.ser_kf.readline().decode('ascii', errors='ignore').strip()  
+            if not (line.startswith("$VNYMR") or line.startswith("$VNSTV")):  
+                self.get_logger().info(f"Set gravity vector response: {response_line}")
+                waiting_for_response = False
 
         # Step 3: Configure async outputs
-        # IMU port → ADOR=253 (VNCMV), ADOF=50 Hz
-        self.ser_imu.write(full_vn_message("VNWRG,6,253"))
-        self.ser_imu.write(full_vn_message("VNWRG,7,50"))
+        # IMU port should write the true(gravity compensated) body-fixed measurements(setting 16) at a rate of 50Hz
+        self.ser_imu.write(full_vn_message("VNWRG,6,16,1"))
+        self.ser_imu.write(full_vn_message("VNWRG,7,50,1"))
 
         # KF port → ADOR=254 (VNSTV), ADOF=50 Hz
-        self.ser_kf.write(full_vn_message("VNWRG,6,254"))
-        self.ser_kf.write(full_vn_message("VNWRG,7,50"))
-
+        self.ser_imu.write(full_vn_message("VNWRG,6,254,2"))
+        self.ser_imu.write(full_vn_message("VNWRG,7,50,2"))
+        
+        self.get_logger().info("PORTS ARE SET. SHOULD SEE NEW MESSAGES NOW")
         # Step 4: Save to non-volatile memory
-        self.ser_imu.write(full_vn_message("VNWNV"))
-        self.ser_kf.write(full_vn_message("VNWNV"))
-
-        response.success = True
-        response.message = "VN100 configured with measured gravity vector and async outputs."
+        #self.ser_imu.write(full_vn_message("VNWNV"))
+        #self.ser_kf.write(full_vn_message("VNWNV"))
+        
+        if not np.mean(avg_accel) == 0: 
+            response.success = True
+            response.message = "VN100 configured with measured gravity vector and async outputs."
         return response
 
 
---
+++ bubble_sensors/src/imu_configurator.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+File: imu_calibrator v1.0
+Author: Henry Adam
+Date: Aug 18, 2025
+
+The purpose of this file is to complete the automated calibration of the vectornav vn100 IMU. 
+In the file, the raw output is 
+"""
+
+import rclpy
+from rclpy.node import Node
+from bubble_sensors.srv import ConfigureVN100
+import serial
+import time
+import numpy as np
+
+# just gets the checksum in order to complete the raw message
+def vn_checksum(payload: str) -> str:
+    x = 0
+    for ch in payload.encode('ascii'):
+        x ^= ch
+    return f"{x:02X}"
+
+# completes the vectornav message with the raw message and checksum
+def full_vn_message(payload: str) -> bytes:
+    cks = vn_checksum(payload)
+    return f"${payload}*{cks}\r\n".encode('ascii')
+
+class VN100Configurator(Node):
+    def __init__(self):
+        super().__init__('imu_calibrator')
+
+        # declare the parameters for the ports and baudrate
+        self.declare_parameter("imu_port", '/dev/ttyAMA5')
+        self.declare_parameter('kf_port', '/dev/ttyAMA4')
+        self.declare_parameter('baudrate', 115200)
+        self.declare_parameter('print_output', False)
+        self.declare_parameter('read_time', 10.0)
+
+        # Configure the two UART ports(imu port for imu, kf port for kalman filter outputs)
+        self.imu_port = self.get_parameter("imu_port").get_parameter_value().string_value 
+        self.kf_port  = self.get_parameter("kf_port").get_parameter_value().string_value  
+        self.baudrate = self.get_parameter("baudrate").get_parameter_value().integer_value
+        self.print_output = self.get_parameter("print_output").get_parameter_value().bool_value
+        self.read_time = self.get_parameter("read_time").get_parameter_value().double_value
+        # some configuration for the ports
+        try: 
+            self.ser_imu = serial.Serial(self.imu_port, self.baudrate, timeout=1)
+            self.ser_kf  = serial.Serial(self.kf_port,  self.baudrate, timeout=1)
+        except Exception as e: 
+            self.get_logger().error(f"ERROR OPENING SERIAL PORTS: {e}")
+
+        # Create service: when called, performs calibration and config
+        self.srv = self.create_service(ConfigureVN100, 'configure_vn100', self.handle_configure)
+
+        self.get_logger().info("VN100 Configurator ready. Call /configure_vn100 service.")
+
+    def handle_configure(self, request, response):
+        # parse the request 
+        port = request.port
+        try: 
+            if port == "imu": 
+                self.ser_imu.write(full_vn_message(request.msg))
+            if port == "kf":
+                self.ser_imu.write(full_vn_message(request.msg))
+        except Exception as e: 
+            self.get_logger().error(f"Error sending the request! Error: {e}") 
+            response.success = False
+            response.message = f"Error in sending the request: {e}"
+            return response
+
+        if self.print_outputs:
+            # now read all of the messages coming in from the port
+            self.get_logger().info("Reading Logs for {self.read_time} seconds")
+            timeout = self.read_time + time.time()
+            try: 
+                while time.time() < timeout: 
+                    if port == "imu": 
+                        line = self.ser_imu.readline().decode('ascii', errors='ignore').strip()  
+                        self.get_logger().info(f"FROM IMU PORT: {line}") 
+                    if port == "kf":
+                        line = self.ser_kf.readline().decode('ascii', errors='ignore').strip() 
+                        self.get_logger().info(f"FROM KF PORT: {line}") 
+            except KeyboardInterrupt: 
+                self.get_logger().warn("KeyboardInterrupt on listening step")
+
+        response.success = True
+        response.message = f"VN100 message sent:d {str(request)}."
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = VN100Configurator()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
